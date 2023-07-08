d5315b71b438a92a4753bc02efd57fd7a21b9730
SnehaJaiswal123 <snehajais270703@gmail.com>
2023-07-08 17:41:33 +0530

    added cors

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const express=require('express');
+const cors=require('cors');
 const Sponsers = require('./src/routes/sponsors');
 const Events = require('./src/routes/events');
 const userSponsor=require('./src/routes/usersponsor')
 const userEvent=require('./src/routes/userevent')
 
+
 const app=express();
 
 app.use(express.json())
+app.use(cors({
+    origin:"*"
+}))
 
 app.use(Sponsers)
 app.use(Events)
