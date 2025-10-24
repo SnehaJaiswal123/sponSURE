520aa67a17ecb242b75c8b01bea11c686f6e18de
SnehaJaiswal123 <snehajais270703@gmail.com>
2025-10-24 14:24:29 +0530

    fixed deployed issue

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,9 +1,1 @@
-const express=require('express')
-const router=express.Router()
-const Events=require('../controller/events')
-const auth=require('../middleware/auth')
 
-router.post('/createevent',auth, Events.createevent)
-router.get('/listsponsors',auth, Events.listSponsor)
-
-module.exports=router
--- src/routes/sponsors.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const router=express.Router()
-const Sponsors=require('../controller/sponsors')
-const auth=require('../middleware/auth')
-
-router.post('/createsponsor',auth,Sponsors.createsponsor)
-router.get('/listevents',auth,Sponsors.listevents)
-
-module.exports=router;
